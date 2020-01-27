f7addbcbf1a21f2757005b006c558841d1ce1def
Liwia <liwia@air-liwia.home>
2020-01-27 15:18:59 +0100

    Updates

---
+++ precache-manifest.28c277cb93dd380609c2727986d0c60c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7732c3b936cb9743a223f3ce7cddcf6",
+    "url": "/web-games/index.html"
+  },
+  {
+    "revision": "0f6b39a26e2ccb19c09b",
+    "url": "/web-games/static/css/2.7a1db334.chunk.css"
+  },
+  {
+    "revision": "43f6685011d107d6e173",
+    "url": "/web-games/static/css/main.d41c1717.chunk.css"
+  },
+  {
+    "revision": "0f6b39a26e2ccb19c09b",
+    "url": "/web-games/static/js/2.ac2d640f.chunk.js"
+  },
+  {
+    "revision": "9224d029b026b0e0730206af5450eec2",
+    "url": "/web-games/static/js/2.ac2d640f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "43f6685011d107d6e173",
+    "url": "/web-games/static/js/main.36180e6a.chunk.js"
+  },
+  {
+    "revision": "92d9faf316548c682411",
+    "url": "/web-games/static/js/runtime-main.791d22e3.js"
+  },
+  {
+    "revision": "a598e0591f3569cf65caf02fb9d92078",
+    "url": "/web-games/static/media/Paper.a598e059.png"
+  },
+  {
+    "revision": "96c595908b7b5a0f2f97e0eacb1b51b8",
+    "url": "/web-games/static/media/Rock.96c59590.png"
+  },
+  {
+    "revision": "5d9a8354282ddd28a18190b2723e5160",
+    "url": "/web-games/static/media/Scissors.5d9a8354.png"
+  },
+  {
+    "revision": "fdd4dd06e8f82bade9dc3c59092ec000",
+    "url": "/web-games/static/media/tlo2.fdd4dd06.jpg"
+  }
+]);
